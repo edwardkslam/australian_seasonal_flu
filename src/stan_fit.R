d0a962bdedcb550e377c672eb2c3d4c288003b23
Dylan Morris <dhmorris@princeton.edu>
2019-06-21 02:08:43 -0400

    Added prelim stan model

---
+++ src/stan_fit.R
@@ -0,0 +1,176 @@
+#!/usr/bin/env Rscript
+
+################################
+## fit the specified stan model
+## to the specified clean dataset,
+## and save the result as an .Rds
+## file
+##
+####################################
+
+suppressPackageStartupMessages(library(rstan))
+suppressPackageStartupMessages(library(parallel))
+suppressPackageStartupMessages(library(readr))
+
+
+## read command line args
+args <- commandArgs(trailingOnly=TRUE)
+model_src_path <- "multilevel_incidence_model"
+datapath <- "../epi_table.csv"
+
+
+## set stan options
+n_cores <- parallel::detectCores()
+options(mc.cores = n_cores)
+rstan_options(auto_write = TRUE)
+niter <- 2000
+nchains = n_cores
+adapt_d= 0.8
+max_tree = 15
+fixed_seed = 232
+
+## load data
+dat <- read_csv(datapath)
+
+#############################
+## format data for model
+############################
+
+library(magrittr)
+library(ggplot2)
+library(ggpubr)
+library(gridExtra)
+library(scales)
+library(dplyr)
+library(plyr)
+library(tidyr)
+library(readr)
+
+#Here we assume and correct for potential mis-identification during antigenic characterisation due to delays in 
+#updating vaccine strain nomenclature.
+
+# The following code will reproduce the prior immunity analyses discussed in the main text:
+# 1)  The relationship between epidemic incidence and the amount of antigenic variant-specific cumulative incidence 
+#     epi_size_cumulative_size_same_variant_plot (Figure 4)
+#
+# 2)  The relationship between the probability of successful epidemic initiation 
+#     and amount of antigenic variant-specific cumulative incidence for each subtype 
+#     prob_successful_epi_cumulative_size_same_variant_plot (Figure S16)
+#
+# 3)  Binary logistic regression assessing the effect of antigenic variant-specific cumulative incidence 
+#     on the probability of successful epidemic initiation for each subtype 
+#     subtype_logistics_regression (Table S6)
+
+# Loading data ------------------------------------------------------------
+
+epi_table = read_csv("../epi_table.csv")
+
+cities<-c("ADELAIDE","BRISBANE","MELBOURNE","PERTH","SYDNEY")
+
+epi_table$city<-factor(epi_table$city,levels = cities)
+
+
+dodgy_prev<-epi_table%>%subset(.,is.na(new_ag_marker) | 
+                                 reference_strain=="A/Perth/16/2009-like")
+
+# First find the first and last year in which an ag variant causes an epidemic in each city
+first_emerge_table<-epi_table%>%
+  subset(.,epi_alarm=="Y")%>%
+  subset(.,!(reference_strain%in%dodgy_prev$reference_strain))%>%
+  dplyr::group_by(city,subtype,reference_strain)%>%
+  dplyr::summarise(emerge_year=min(year),
+                   last_recorded_epi=max(year))
+
+# assume that an ag variant could potentially have caused an epidemic right up to the year 
+# before the emergence of its replacement new variant
+first_emerge_table<-first_emerge_table%>%
+  dplyr::group_by(city,subtype)%>%
+  dplyr::arrange(emerge_year,.by_group=TRUE)%>%
+  dplyr::mutate(last_possible_year=if(subtype!="H1sea"){lead(emerge_year-1,default = 2015)}else{lead(emerge_year-1,default = 2008)})
+
+# the last possible year in which an epidemic could have been caused is whichever occured last:
+# the last recorded epidemic (to account for the case in which old and new variants circulate in same year)
+# the year prior to emergence of new variant
+first_emerge_table<-first_emerge_table%>%
+  dplyr::rowwise()%>%
+  dplyr::mutate(last_possible_year=max(last_possible_year,last_recorded_epi))
+
+# correcting for mislabelling 
+end_rows<-which(first_emerge_table$subtype=="H1sea" & first_emerge_table$last_possible_year>=2009)
+first_emerge_table$last_possible_year[end_rows]<-2008
+
+# generate the full list of possible years for each variant
+cumulative_incidence_by_ag<-first_emerge_table%>%
+  dplyr::group_by(city,subtype,reference_strain)%>%
+  tidyr::expand(.,year=full_seq(c(emerge_year,last_possible_year),1))
+
+cumulative_incidence_by_ag<-cumulative_incidence_by_ag%>%
+  dplyr::group_by(city,reference_strain)%>%
+  dplyr::mutate(years_since_emergence=year-min(year))
+
+# filling in which are the years with epidemics and their sizes
+cumulative_incidence_by_ag<-left_join(cumulative_incidence_by_ag,epi_table[,c("city","subtype","reference_strain","year","incidence_per_mil")])
+cumulative_incidence_by_ag<-rename(cumulative_incidence_by_ag,replace=c("incidence_per_mil"= "incidence_current_season"))
+cumulative_incidence_by_ag<-data.frame(cumulative_incidence_by_ag)
+cumulative_incidence_by_ag$incidence_current_season[which(is.na(cumulative_incidence_by_ag$incidence_current_season))]<-0
+
+# add cumulative incidence for each variant
+cumulative_incidence_by_ag<-cumulative_incidence_by_ag%>%
+  dplyr::group_by(city,subtype,reference_strain)%>%
+  dplyr::arrange(year,.by_group=TRUE)%>%
+  dplyr::mutate(cumulative_prior_incidence_same_ag = cumsum(incidence_current_season)- incidence_current_season)
+
+# need to divide cumulative incidence by city-specific mean epidemic size
+overall_mean_size<-epi_table%>%
+  subset(.,epi_alarm=="Y")%>%
+  dplyr::group_by(city)%>%
+  dplyr::summarise(mean_epi_size=mean(incidence_per_mil))
+
+cumulative_incidence_by_ag<-cumulative_incidence_by_ag%>%left_join(overall_mean_size)
+
+cumulative_incidence_by_ag$standardised_prior_cumulative<-cumulative_incidence_by_ag$cumulative_prior_incidence_same_ag/cumulative_incidence_by_ag$mean_epi_size
+cumulative_incidence_by_ag$standardised_current_season<-cumulative_incidence_by_ag$incidence_current_season/cumulative_incidence_by_ag$mean_epi_size
+
+dat<-cumulative_incidence_by_ag%>%left_join(data.frame(city=epi_table$city,reference_strain=epi_table$reference_strain,year=epi_table$year,epi_alarm=factor(epi_table$epi_alarm,levels = c("Y","N"))))
+cumulative_incidence_by_ag$epi_alarm[is.na(cumulative_incidence_by_ag$epi_alarm)==TRUE]<-"N"
+
+
+
+## make data into list
+data_list <- list(
+    n_cities=length(unique(dat$city)),
+    n_epidemics_total=length(dat$incidence_per_mil),
+    incidences=log(dat$incidence_per_mil),
+    city=dat$city,
+    antigenic_change=dat$new_ag_marker,
+    abs_humidity=dat$avg_abs_humidity,
+    prior_activity=dat$prior_everything_scaled,
+    cumulative_prior_incidence=)
+    
+
+# concatenate with prior choices
+stan_data = c(
+    main_hyperprior_list,
+    multilevel_hyperprior_list,
+    gamma_hyperprior_list,
+    regression_hyperprior_list,
+    data_list)
+
+###############################
+## Compile, fit, and save model
+###############################
+fit <- stan(
+    model_src_path,
+    data=stan_data,
+    iter=niter,
+    seed=fixed_seed,
+    chains=nchains, 
+    control = list(max_treedepth = max_tree,
+                   adapt_delta = adapt_d))
+
+cat(sprintf("saving posterior samples to file %s...\n",
+            outpath))
+
+saveRDS(fit, outpath)
+
+warnings()
